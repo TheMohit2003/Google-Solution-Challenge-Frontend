765b16da1575781bbd3e7b0a476d66725fbf9e53
Ishika-08 <ishikaranjan8@gmail.com>
2024-02-18 19:37:36 +0530

    added some styles

--- src/Components/LandingPage/Features.jsx
+++ src/Components/LandingPage/Features.jsx
@@ -44,26 +44,26 @@ export default function Features() {
       
   return (
   <div>
-    <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-20">
-      <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Features</h2>
-      <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Empowering Your Service Journey</h1>
+    <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-col text-center w-full mb-20">
+      <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Features</h2>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Empowering Your Service Journey</h1>
     </div>
-    <div class="flex flex-wrap -m-4 lg:w-max-5xl mx-auto">
+    <div className="flex flex-wrap -m-4 lg:w-max-5xl mx-auto">
     {featureCards.map(ele =>(
-        <div class="p-4 md:w-1/3">
-        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-          <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+        <div className="p-4 md:w-1/3" key={ele.id}>
+        <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+          <div className="flex items-center mb-3">
+            <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                 <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
               </svg>
             </div>
-            <h2 class="text-gray-900 text-lg title-font font-medium">{ele.title}</h2>
+            <h2 className="text-gray-900 text-lg title-font font-medium">{ele.title}</h2>
           </div>
-          <div class="flex-grow">
-            <p class="leading-relaxed text-base">{ele.info}</p>
+          <div className="flex-grow">
+            <p className="leading-relaxed text-base">{ele.info}</p>
           </div>
         </div>
       </div>
