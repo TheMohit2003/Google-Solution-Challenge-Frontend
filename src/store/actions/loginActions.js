a2befe9b78df11576c46f91aad64486b69ae42ff
Restyled.io <commits@restyled.io>
2024-02-19 07:26:15 +0000

    Restyled by clang-format

--- src/store/actions/loginActions.js
+++ src/store/actions/loginActions.js
@@ -1,7 +1,7 @@
 export const SAVE_NAME = 'SAVE_NAME';
 export const saveName = (name) => {
-    return {
-        type: 'SAVE_NAME',
-        payload: name,
-    };
+  return {
+    type : 'SAVE_NAME',
+    payload : name,
+  };
 };
--- src/store/reducers/loginReducer.js
+++ src/store/reducers/loginReducer.js
@@ -1,14 +1,14 @@
 const initialState = {
-    name: '',
+  name : '',
 };
 
 const loginReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'SAVE_NAME':
-            return { ...state, name: action.payload };
-        default:
-            return state;
-    }
+  switch (action.type) {
+  case 'SAVE_NAME':
+    return {...state, name : action.payload};
+  default:
+    return state;
+  }
 };
 
 export default loginReducer;
