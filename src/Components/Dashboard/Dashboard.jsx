2853bb62f14e562ec0ad2e355d92ac1e7e343669
apuu1503 <apurvasuakle2003@gmail.com>
2024-02-19 12:52:50 +0530

    redux and dashboard layout

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,27 @@
+// App.jsx
+import React from 'react';
+import { Layout } from 'antd';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Services from './Services';
+
+const { Content } = Layout;
+
+const Dashboard = () => {
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Navbar />
+            <Layout>
+                <Sidebar />
+                <Layout>
+                    <Content style={{ padding: '20px' }}>
+                        <Services />
+                        <p>This is your main content.</p>
+                    </Content>
+                </Layout>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default Dashboard;
---
+++ src/store/reducers/loginReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    name: '',
+};
+
+const loginReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SAVE_NAME':
+            return { ...state, name: action.payload };
+        default:
+            return state;
+    }
+};
+
+export default loginReducer;
