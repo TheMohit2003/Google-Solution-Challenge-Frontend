fa5fae409f15869f7ca3803732e56ff3075626a0
Ishika-08 <ishikaranjan8@gmail.com>
2024-02-20 13:27:19 +0530

    started Issuer Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./Pages/Sign";
 import LogIn from "./Pages/LogIn";
 import LandingPage from "./Pages/LandingPage";
+import IssuerDashboard from "./Pages/IssuerDashboard";
 
 import Dashboard from "./Components/Dashboard/Dashboard";
 
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/log-in" element={<LogIn />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/issuer-dashboard" element={<IssuerDashboard/>}/>
       </Routes>
     </Router>
   );
