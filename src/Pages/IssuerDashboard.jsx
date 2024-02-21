a78b3ea37406ecfe11a95de71ba40dd200960581
Ishika-08 <ishikaranjan8@gmail.com>
2024-02-21 14:20:38 +0530

    added vercel.json

--- src/Pages/IssuerDashboard.jsx
+++ src/Pages/IssuerDashboard.jsx
@@ -1,24 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "antd";
 import Navbar from "../Components/IssuerDashboard/Navbar";
 import Sidebar from "../Components/IssuerDashboard/Sidebar";
-import BreadCrumb from "../Components/IssuerDashboard/BreadCrumb";
-import NewBidCard from "../Components/IssuerDashboard/NewBidCard";
-import AllBids from "../Components/IssuerDashboard/AllBids";
+import BreadCrumb from "../Components/IssuerDashboard/Dashboard/BreadCrumb";
+import NewBidCard from "../Components/IssuerDashboard/Dashboard/NewBidCard";
+import AllBids from "../Components/IssuerDashboard/Dashboard/AllBids";
+import Profile from "../Components/IssuerDashboard/Profile";
+import Stopwatch from "../Components/IssuerDashboard/Stopwatch"
 const { Content } = Layout;
 
 const Dashboard = () => {
+  const [selectedMenuItem, setSelectedMenuItem] = useState("dashboard");
+
+  const handleMenuItemSelect = (menuItem) => {
+    setSelectedMenuItem(menuItem);
+  };
+
+  // Render content based on selected menu item
+  const renderContent = () => {
+    switch (selectedMenuItem) {
+      case "dashboard":
+        return (
+          <>
+            <BreadCrumb />
+            <NewBidCard />
+            <AllBids />
+          </>
+        );
+      case "profile":
+        return <Profile />;
+      case "LiveBids":
+        return (
+          <>
+            <Stopwatch/>
+          </>
+        )
+      default:
+        return null;
+    }
+  };
+
   return (
     <Layout style={{ minHeight: "100vh" }}>
       <Navbar />
       <Layout>
-        <Sidebar />
+        <Sidebar onMenuItemSelect={handleMenuItemSelect} />
         <Layout>
-          <Content style={{ padding: "20px" }}>
-            <BreadCrumb/>
-            <NewBidCard/>
-            <AllBids/>
-          </Content>
+          <Content style={{ padding: "20px" }}>{renderContent()}</Content>
         </Layout>
       </Layout>
     </Layout>
@@ -26,3 +54,5 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+
